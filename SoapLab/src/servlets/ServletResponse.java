58be8bd62f8f77bde9b1472d64dd84d2987f1464
jwatson <jwatson@student.neumont.edu>
2013-08-12 03:09:44 -0600

    SoapLab Done

--- SoapLab/src/servlets/ServletResponse.java
+++ SoapLab/src/servlets/ServletResponse.java
@@ -1,10 +1,15 @@
 package servlets;
 
+import com.sun.jmx.remote.internal.Unmarshal;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -15,27 +20,19 @@ import java.util.Scanner;
  * Created with IntelliJ IDEA.
  * User: Michael
  * Date: 8/12/13
- * Time: 12:00 AM
+ * Time: 2:59 AM
  * To change this template use File | Settings | File Templates.
  */
-@WebServlet("/ServletSoap")
-public class ServletSoap extends HttpServlet {
+@WebServlet(name = "ServletResponse")
+public class ServletResponse extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Scanner scan;
-        scan = null;
+        JAXBContext jc = null;
         try {
-            URL test = (this.getClass().getClassLoader().getResource("Resources/restaurant-soap-response.xml"));
-            scan = new Scanner(new File(this.getClass().getClassLoader().getResource("restaurant-soap-response.xml").toURI()));
-        } catch (URISyntaxException e) {
+            jc = JAXBContext.newInstance("");
+            Unmarshaller u = jc.createUnmarshaller();
+        } catch (JAXBException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        String xml ="";
-        while(scan.hasNext()){
-            xml+=scan.nextLine();
-        }
-        response.setContentLength(xml.length());
-        response.setContentType("text/xml");
-        response.getWriter().write(xml);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
